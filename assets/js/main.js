a91563451d0f8c37e0e055d72ee35ead530bb2a6
RevTK <trainst37@gmail.com>
2023-03-24 12:45:24 +0900

    페이징 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,7 @@ let opt = {
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
   jsPDF:        { format: 'a4', orientation: 'portrait' },
-  pagebreak: { after:[]}
+  pagebreak: { after:['.page2']}
 };
 
 
